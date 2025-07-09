bfccb4a64bb4f287d68bea66e328035214b4c31a
Isabelle <isasoarez21@gmail.com>
2025-07-08 21:16:27 -0300

    feat: add side colliders to the tile to limit the player

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -7,13 +7,14 @@ public class PlayerCollision : MonoBehaviour
     //audio
     public AudioSource audioeffecthit;
 
+    public Rigidbody rb;
     //pegar o go menu
     public GameObject menuGameOver;
 
     public GameObject scorePainel;
 
     //pegar o script do palyer(que eu não sabia como fazer)
-    public PlayerMovimentt playermov;
+    public PlayerMoviment playermov;
   
     //estrutura tradicional de colisão assim como usado nas outras vezes
    void OnCollisionEnter(Collision collision)
@@ -24,16 +25,37 @@ public class PlayerCollision : MonoBehaviour
                 playermov.enabled = false;
             }
 
-            audioeffecthit.Play();
-
-            scorePainel.SetActive(false);
-
-            menuGameOver.SetActive(true);
-            
-            //para o tempo
-            Time.timeScale = 1f;
-
+            Died();
         }
         
    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ColliderLateral"))
+        {
+            if (playermov != null)
+            {
+                playermov.enabled = false;
+            }
+            Died();
+        }
+    }
+
+
+    private void Died()
+    {   
+        rb.isKinematic = true;
+
+        rb.velocity = Vector3.zero;
+
+        audioeffecthit.Play();
+
+        scorePainel.SetActive(false);
+
+        menuGameOver.SetActive(true);
+
+        //para o tempo
+        Time.timeScale = 1f;
+    }
 }
